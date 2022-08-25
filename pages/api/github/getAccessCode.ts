48bd4252c403e75735014b9236ce9d27803a03ce
leojacoby <leojacoby@gmail.com>
2022-08-25 14:28:11 -0400

    works!

--- pages/api/github/getAccessCode.ts
+++ pages/api/github/getAccessCode.ts
@@ -1,22 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import axios from 'axios';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from 'utils/constants';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+import axios from 'axios'
 
-  const code = req.query.access_code as string;
-  try {
-    const response = await axios.post(
-      `http://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${code}`
-    );
-    console.log('RESPONSE', response);
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from 'utils/constants'
 
-    res.status(200).json(response);
-  } catch (err) {
-    console.log('github err', err);
-    res.status(500).json({ error: 'BAD' });
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const code = req.query.access_code as string
+    try {
+        const { data: token } = await axios.post(
+            `http://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${code}`,
+        )
+
+        // TODO: store token in DB
+
+        res.status(200).json({ success: true })
+    } catch (err) {
+        console.log('github err', err)
+        res.status(500).json({ error: 'BAD' })
+    }
 }
