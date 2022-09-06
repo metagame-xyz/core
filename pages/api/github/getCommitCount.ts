8ae05f2c971fcad96550a30e3f39ea80529b178e
leojacoby <leojacoby@gmail.com>
2022-09-06 16:25:48 -0400

    Update getCommitCount.ts

--- pages/api/github/getCommitCount.ts
+++ pages/api/github/getCommitCount.ts
@@ -35,6 +35,9 @@ export default async function handler(req: any, res: NextApiResponse) {
     const contributions = (response?.data?.user?.contributionsCollection?.commitContributionsByRepository || []).filter(
         (contribution) => contribution?.repository?.name === repo,
     )
+
+    console.log(contributions)
+
     if (!contributions || !contributions.length) {
         return res.status(200).json(0)
     }
