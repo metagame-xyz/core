b7ab16f6e985a4070d99f2ce2ff43ddd606021c5
Brenner Spear <blspear@gmail.com>
2022-09-28 15:53:45 -0400

    clean up contstants file

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,47 +1,48 @@
-// export const NETWORK = process.env.NETWORK;
+// RPC API Keys
 export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-// export const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
-// export const FTMSCAN_API_KEY = process.env.FTMSCAN_API_KEY;
-// export const SNOWTRACE_API_KEY = process.env.SNOWTRACE_API_KEY;
+export const POCKET_NETWORK_API_KEY = process.env.POCKET_NETWORK_API_KEY
+export const POCKET_NETWORK_ID = process.env.POCKET_NETWORK_ID
+export const INFURA_PROJECT_ID = process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
+export const ALCHEMY_PROJECT_ID = process.env.NEXT_PUBLIC_ALCHEMY_PROJECT_ID
 
-export const REDIS_URL = process.env.REDIS_URL
+// Other API Keys
+export const ALCHEMY_NOTIFY_TOKEN = process.env.ALCHEMY_NOTIFY_TOKEN
+export const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY
+
+// Metagame specific constants
 export const EVENT_FORWARDER_AUTH_TOKEN = process.env.EVENT_FORWARDER_AUTH_TOKEN
 export const EVENT_FORWARDER_AUTH_TOKEN_HEADER = 'x-event-forwarder-auth-token'
+export const METABOT_BASE_API_URL = process.env.METABOT_BASE_API_URL
+export const WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL
+export const VALIDATOR_PRIVATE_KEY = process.env.VALIDATOR_PRIVATE_KEY
+export const THE_METAGAME_ETH_ADDRESS = '0x902A37155438982884ca26A5DBccf73f5ae8194b'
+
+// Datastore constants
+export const METABOT_DB_CONNECTION_STRING = process.env.METABOT_DB_CONNECTION_STRING
+export const EVM_TRANSLATOR_CONNECTION_STRING = process.env.EVM_TRANSLATOR_CONNECTION_STRING
+
 export const INFURA_IPFS_PROJECT_ID = process.env.INFURA_IPFS_PROJECT_ID
 export const INFURA_IPFS_SECRET = process.env.INFURA_IPFS_SECRET
 export const INFURA_IPFS_PROJECT_ID_HEADER = `x-ipfs-project-id`
 export const INFURA_IPFS_SECRET_HEADER = `x-ipfs-project-secret`
-export const POCKET_NETWORK_API_KEY = process.env.POCKET_NETWORK_API_KEY
-export const POCKET_NETWORK_ID = process.env.POCKET_NETWORK_ID
-export const ALCHEMY_NOTIFY_TOKEN = process.env.ALCHEMY_NOTIFY_TOKEN
-export const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY
-export const DATADOG_API_KEY = process.env.DATADOG_API_KEY
-export const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN
-export const METABOT_BASE_API_URL = process.env.METABOT_BASE_API_URL
 
+// Logging
+export const DATADOG_API_KEY = process.env.DATADOG_API_KEY
 export const DATADOG_RUM_APPLICATION_ID = process.env.NEXT_PUBLIC_DATADOG_RUM_APPLICATION_ID
 export const DATADOG_RUM_CLIENT_TOKEN = process.env.NEXT_PUBLIC_DATADOG_RUM_CLIENT_TOKEN
 export const DATADOG_RUM_ENV = process.env.NEXT_PUBLIC_DATADOG_RUM_ENV
 
-export const METABOT_DB_CONNECTION_STRING = process.env.METABOT_DB_CONNECTION_STRING
-export const EVM_TRANSLATOR_CONNECTION_STRING = process.env.EVM_TRANSLATOR_CONNECTION_STRING
-
+// Prod vs Dev constants
 export const isProdEnv = process.env.NODE_ENV === 'production'
-
-/* Frontend Constants */
 export const NETWORK = process.env.NEXT_PUBLIC_NETWORK?.toLowerCase()
-export const INFURA_PROJECT_ID = process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
-export const ALCHEMY_PROJECT_ID = process.env.NEXT_PUBLIC_ALCHEMY_PROJECT_ID
-// export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-
-// heartbeat.loca.lt, logbook-dev.themetagame.xyz, logbook.themetagame.xyz
-export const WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL
 export const networkStrings = getNetworkString(NETWORK)
-export const THE_METAGAME_ETH_ADDRESS = '0x902A37155438982884ca26A5DBccf73f5ae8194b'
-export const VALIDATOR_PRIVATE_KEY = process.env.VALIDATOR_PRIVATE_KEY
 
+// Network specific contract addresses
 export const NOMAD_WHITEHAT_CONTRACT_ADDRESS = process.env.NOMAD_WHITEHAT_CONTRACT_ADDRESS
 export const LOGBOOK_CONTRACT_ADDRESS = process.env.LOGBOOK_CONTRACT_ADDRESS
+
+/* Frontend Constants */
+// export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 type NetworkStrings = {
     alchemy: string
