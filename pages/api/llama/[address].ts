ccf115a35a00f9ca9c18ce45fdb6cbe1a424256c
Brenner Spear <blspear@gmail.com>
2022-10-23 23:53:50 -0400

    canvas image generation

--- pages/api/llama/[address].ts
+++ pages/api/llama/[address].ts
@@ -2,110 +2,24 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { AddressZ } from 'evm-translator'
 import _ from 'lodash'
-import { AssetData, CriteriaMap, LayerItemData, LayerItemRow, UserData } from 'types'
-import { IncomingLlamaUserData, LlamaTier, TierToLevel } from 'types/llama'
 
 import { validateLlamaPfpAllowList } from 'api/premintCheck/checks/llama'
 
-import { fetcher } from 'utils/requests'
+import { getLlamaUserData, layerItemRowsToAssetData, llamaCriteriaMap } from 'utils/llama'
 
 import { allRows } from './assetData'
 
 const token = ''
 
-const getMaxTier = (tiersBySeason: Record<number, LlamaTier>) => {
-    const tiers = Object.values(tiersBySeason)
-    const maxTier = tiers.reduce((acc: string, curr: string) => {
-        if (TierToLevel[curr] > TierToLevel[acc]) {
-            return curr
-        }
-        return acc
-    }, 'Traveler') as string
-    return maxTier
-}
-
-const llamaBackgroundCriteriaChecker = (criteriaRequirement: string) => (llamaUserData: IncomingLlamaUserData) => {
-    const llamaLevel = TierToLevel[llamaUserData.tier]
-    const criteriaLevel = TierToLevel[criteriaRequirement]
-    return llamaLevel >= criteriaLevel
-}
-
-const llamaNecklaceCriteriaChecker = (criteriaRequirement: string) => (llamaUserData: IncomingLlamaUserData) => {
-    const maxTier = getMaxTier(llamaUserData.tier_by_season)
-    const maxTierLevel = TierToLevel[maxTier]
-    const criteriaLevel = TierToLevel[criteriaRequirement]
-    return maxTierLevel >= criteriaLevel
-}
-
-const llamaCriteriaMap: CriteriaMap = {
-    Background: {
-        Traveler: llamaBackgroundCriteriaChecker(LlamaTier.Traveler),
-        Explorer: llamaBackgroundCriteriaChecker(LlamaTier.Explorer),
-        Mountaineer: llamaBackgroundCriteriaChecker(LlamaTier.Mountaineer),
-        Rancher: llamaBackgroundCriteriaChecker(LlamaTier.Rancher),
-    },
-    Necklace: {
-        Traveler: llamaNecklaceCriteriaChecker(LlamaTier.Traveler),
-        Explorer: llamaNecklaceCriteriaChecker(LlamaTier.Explorer),
-        Mountaineer: llamaNecklaceCriteriaChecker(LlamaTier.Mountaineer),
-        Rancher: llamaNecklaceCriteriaChecker(LlamaTier.Rancher),
-    },
-}
-
-const layerItemRowToLayerItem = (
-    layerItemRow: LayerItemRow,
-    userData: IncomingLlamaUserData,
-    criteriaMap: CriteriaMap,
-): LayerItemData => {
-    const { name, pngLink, earnedDescription, earnedCriteria, category } = layerItemRow
-
-    // if (category === 'Background') {
-    //     console.log(earnedCriteria)
-    //     console.log(criteriaMap?.[category]?.[earnedCriteria]?.(userData))
-    // }
-    return {
-        name,
-        pngLink,
-        earnedDescription,
-        earned: criteriaMap?.[category]?.[earnedCriteria]?.(userData) ?? null,
-    }
-}
-
-const layerItemRowsToAssetData = (
-    layerRows: LayerItemRow[],
-    userData: IncomingLlamaUserData,
-    criteriaMap: CriteriaMap,
-): AssetData => {
-    // loop through them all to get the earned status of each
-
-    // group by category
-    const layerRowsByCategoryMap = _(layerRows).groupBy('category')
-
-    // filter keys, add earned boolean
-    const assetData = layerRowsByCategoryMap.map((val, key) => ({
-        category: key,
-        modifiable: val[0].modifiable,
-        options: val.map((obj) => layerItemRowToLayerItem(obj, userData, criteriaMap)),
-    }))
-
-    return assetData.value()
-}
-
-const llamaUsersBaseUrl = 'https://community.llama.xyz/api/users/'
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         // const address = req.query.address as string
-        const authToken = req.query.jwt || (token as string)
+        const authToken = (req.query.jwt || token) as string
         const llamaUserId = req.query.llamaUserId as string
 
         let llamaData = null
         try {
-            llamaData = (
-                await fetcher(llamaUsersBaseUrl + llamaUserId, {
-                    headers: { authorization: `Bearer ${authToken}` },
-                })
-            ).body
+            llamaData = await getLlamaUserData(llamaUserId, authToken)
 
             console.log('llamaData', llamaData)
         } catch (e) {
@@ -126,13 +40,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const parseData = AddressZ.safeParse(llamaData.eth_login_address)
         const address = parseData.success ? parseData.data : null
 
-        const testData = layerItemRowsToAssetData(allRows, llamaData, llamaCriteriaMap)
+        const assetData = layerItemRowsToAssetData(allRows, llamaData, llamaCriteriaMap)
         const checkResponse = await validateLlamaPfpAllowList(address)
 
         // TODO: get this from the Metatgame database
         // const userData: UserData = {
 
-        return res.status(200).json({ assetData: testData, checkResponse })
+        return res.status(200).json({ assetData, checkResponse })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ error: error.message })
--- pages/api/llama/updatePfp.ts
+++ pages/api/llama/updatePfp.ts
@@ -1,5 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { createCanvas, loadImage } from 'canvas'
+import { AssetData, LayerItemData } from 'types'
+
+import { getLlamaUserData, layerItemRowsToAssetData, llamaCriteriaMap } from 'utils/llama'
+
+import { allRows } from './assetData'
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         /**
@@ -28,8 +35,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
      * including a body shaped like this:
      *
      */
-    const exampleBody: { jwt: string; layers: RequestedPfpLayers } = {
+    const exampleBody: { jwt: string; llamaUserId: string; layers: RequestedPfpLayers } = {
         jwt: 'abc123',
+        llamaUserId: 'e209a559-41e8-4e45-b71e-fb10604e0107',
         layers: [
             { category: 'Background', name: 'Sparkly' },
             { category: 'Necklace', name: 'Green' },
@@ -44,17 +52,64 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     /****************/
     /*     AUTH     */
     /****************/
-    const { jwt, layers } = req.body
+    const { jwt, layers, llamaUserId } = req.body
 
     // use JWT to get Address, ENS, etc from llama backend
 
-    if (jwt === 'check against llama backend') {
-        const error = 'invalid Llama user or jwt'
-        // logger.error({ error }); TODO
-        return res.status(400).send({ error })
+    let incomingUserData = null
+    try {
+        incomingUserData = await getLlamaUserData(llamaUserId, jwt)
+    } catch (e) {
+        if (e.status === 401) {
+            return res.status(401).json({ error: 'Unauthorized' })
+        } else {
+            console.log('Error!')
+            console.log(e.status)
+        }
+    }
+
+    if (!incomingUserData) {
+        return res.status(404).json({ error: 'Llama user not found' })
     }
 
     // check if this user is allowed to use the layers they've requested
+    const assetData = layerItemRowsToAssetData(allRows, incomingUserData, llamaCriteriaMap)
+
+    const getLayer = (assetData: AssetData, layer: { category: string; name: string }): LayerItemData | undefined => {
+        return assetData
+            .find((asset) => asset.category === layer.category)
+            ?.options.find((option) => option.name === layer.name && option.earned !== false)
+    }
+
+    for (const layer of layers) {
+        const matchingAsset = getLayer(assetData, layer)
+
+        if (!matchingAsset) {
+            return res.status(400).json({ error: 'Invalid layer' })
+        }
+    }
+
+    // generate the multi-layer image using canvas
+
+    const canvas = createCanvas(2400, 2400)
+    const ctx = canvas.getContext('2d')
+
+    for (const layer of layers) {
+        const matchingAsset = getLayer(assetData, layer)
+        if (matchingAsset) {
+            const image = await loadImage(matchingAsset.pngLink)
+            ctx.drawImage(image, 0, 0, 2400, 2400)
+        }
+    }
+
+    // return res.send(canvas.toBuffer('image/png'))
+    return res.status(200).json({ image: canvas.toDataURL() })
+
+    // upload the image to IPFS, return hash
+
+    // generate the metadata
+
+    // save the layers to the Metagame database
 
     // return res.status(200).send({updated: true})
 }
