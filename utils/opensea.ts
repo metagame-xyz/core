b3c6e2637a03d1c98571c592715055fc54af28db
Brenner Spear <blspear@gmail.com>
2022-11-02 18:00:02 -0400

    opensea metadata update

--- utils/opensea.ts
+++ utils/opensea.ts
@@ -23,10 +23,20 @@ export type OpenSeaMetadata = {
 // const titleCaseEveryWord = (str: string) =>
 //     str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())
 
+const timestampToDateInSeconds = (timestamp: string) => {
+    const date = new Date(timestamp)
+    return Math.floor(date.getTime() / 1000)
+}
+
 export const metadataToOpenSeaMetadata = (metadata: NftMetadata): OpenSeaMetadata => {
     const extraAsAttributes = getEntries(metadata.extra).map(([key, value]) => ({ trait_type: key, value }))
     const layersAsAttributes = getEntries(metadata.layers).map(([key, value]) => ({ trait_type: key, value }))
-    const attributes = [...extraAsAttributes, ...layersAsAttributes]
+    const attributes = [
+        ...extraAsAttributes,
+        ...layersAsAttributes,
+        { trait_type: 'address', value: metadata.address },
+        { display_type: 'date', trait_type: 'Last Updated', value: timestampToDateInSeconds(metadata.timestamp) },
+    ]
 
     const openseaMetadata: OpenSeaMetadata = {
         name: metadata.name,
