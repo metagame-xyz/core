2d3afcf98895e581e4add932fda4867b2baed9d0
Brenner Spear <blspear@gmail.com>
2022-11-23 13:50:13 -0600

    unhardcode llama contract address

--- pages/api/contract-metadata/[project].ts
+++ pages/api/contract-metadata/[project].ts
@@ -6,15 +6,30 @@ import { copy } from 'utils/content'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const project = req.query.project as string
 
-    // TODO project data for each project. This will show up on the opensea page. Can probably hard code for now but will need to be migrated to DB with a UI to let communities update it themselves.
-    const metadata = {
-        name: `${project} NFT`,
-        description: ``,
-        image: `https://${WEBSITE_URL}/logo.png`,
-        external_link: `https://${WEBSITE_URL}`,
-        seller_fee_basis_points: 800, // 8%,
-        fee_recipient: THE_METAGAME_ETH_ADDRESS,
+    let metadata = null
+
+    // seller_fee_basis_points: 800 // 8%
+
+    if (project === 'llamaPfp') {
+        metadata = {
+            name: `Llama Avatars`,
+            description: ``,
+            // image: `https://${WEBSITE_URL}/logo.png`,
+            // external_link: `https://${WEBSITE_URL}`,
+            seller_fee_basis_points: 0,
+            fee_recipient: '0Xa519A7Ce7B24333055781133B13532Aeabfac81B', // llamacommunity.eth
+        }
     }
+
+    // TODO project data for each project. This will show up on the opensea page. Can probably hard code for now but will need to be migrated to DB with a UI to let communities update it themselves.
+    // const metadata = {
+    //     name: `${project} NFT`,
+    //     description: ``,
+    //     image: `https://${WEBSITE_URL}/logo.png`,
+    //     external_link: `https://${WEBSITE_URL}`,
+    //     seller_fee_basis_points: 800, // 8%,
+    //     fee_recipient: THE_METAGAME_ETH_ADDRESS,
+    // }
 
     res.send(metadata)
 }
